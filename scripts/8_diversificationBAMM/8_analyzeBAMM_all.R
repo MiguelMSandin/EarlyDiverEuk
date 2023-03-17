369bbae85b84fc8721208c0e2003b8bd5e576338
MiguelMSandin <miguelmendezsandin@gmail.com>
2023-03-17 14:45:50 +0100

    Update 8_analyzeBAMM_all.R

--- scripts/8_diversificationBAMM/8_analyzeBAMM_all.R
+++ scripts/8_diversificationBAMM/8_analyzeBAMM_all.R
@@ -44,12 +44,13 @@ for(file in files$files){
 };rm(file, tmp); cat("\n")
 
 # Create another variable for minimum and maximum diversity
-tmp = c()
-for(cl in unique(data$clade)){
-	ss = subset(data, clade==cl)
-	tmp = c(tmp, ifelse(ss$diversity==min(ss$diversity), "min", "max"))
-}; rm(cl, ss)
-data$div = tmp; rm(tmp)
+data$div = NA
+for(clad in unique(data$clade)){
+	ss = subset(data, clade==clad)
+	m = mean(unique(ss$diversity))
+	data$div[which(data$clade==clad & data$diversity < m)] = "min"
+	data$div[which(data$clade==clad & data$diversity > m)] = "max"
+}; rm(clad, ss, m)
 
 # Create a variable for the time point sampled per file
 data$timePoint = rep(seq(1:100), length(unique(data$file))*length(unique(data$rate)))
@@ -108,13 +109,10 @@ pdf(paste0(files$dirPlots, "/", files$RTTplot), width=11.69, height=8.27, paper=
 
 rm(list=ls()[!ls() %in% c("geo", "files")])
 
-files$trees = grep("clade_.*\\.tre", dir(files$dirTrees), value=TRUE)
-files
-
-# Read datasets
+# Reading all shift files
 data = data.frame()
 for(file in files$shifts){
-	cat("\r  Working on file ", file, " (", grep(file, files$shifts), "/", length(files$shifts), ")          ", sep="")
+	cat("\r  Working on file (", grep(file, files$shifts), "/", length(files$shifts), ") ", file, "                    ", sep="")
 	tmp = fread(file)
 	tmp$file = file
 	tmp$clade = file %>% sub(".*diversification_", "", .) %>% sub("_div.*", "", .)
@@ -123,21 +121,14 @@ for(file in files$shifts){
 }; rm(file, tmp); cat("\rDone", rep(" ", 100), "\n")
 
 # Create another variable with minimum and maximum diversity
-
-
-
-
-tmp = c()
+data$div = NA
 for(clad in unique(data$clade)){
 	ss = subset(data, clade==clad)
 	m = mean(unique(ss$diversity))
-	tmp = c(tmp, ifelse(ss$diversity < m, "min", "max"))
+	data$div[which(data$clade==clad & data$diversity < m)] = "min"
+	data$div[which(data$clade==clad & data$diversity > m)] = "max"
 }; rm(clad, ss, m)
-data$div = tmp; rm(tmp)
-
-
-
-
+# tmp=unique(select(data, c("file", "clade", "diversity", "div")))
 
 # Estimate the shifts on diversification rate
 # lam(t) = lam1 * exp(lam2 * t)
@@ -145,7 +136,7 @@ data$div = tmp; rm(tmp)
 data$diversification = data$lam1 - data$mu2
 
 # Get whether the shift corresponds to a decay or a growth
-change = c()
+data$change = NA
 for(f in unique(data$file)){
 	cat("\r  Working on (", grep(f, unique(data$file)), "/", length(unique(data$file)), ") ", f, "                    ", sep="")
 	dirRoot = f %>% sub("\\/[^\\/]+$", "", .) %>% sub("bamm/.*", "", .)
@@ -153,48 +144,66 @@ for(f in unique(data$file)){
 	ss = subset(data, file==f)
 	tree = files$trees %>% grep(dirRoot, ., value=TRUE) %>% grep(clade, ., value=TRUE)
 	tree = read.tree(tree)
-	changeF = c("root")
-	for(n in 2:length(ss$node)){
-		noden = ss$node[n]
-		dn = subset(ss, node==noden)$diversification
-		ancestors = Ancestors(tree, n)
-		parent = max(ancestors[ancestors %in% ss$node])
-		dp = subset(ss, node==parent)$diversification
-		tmp = ifelse(dn > dp, "growth", "decay")
-		changeF = c(changeF, tmp)
+	for(n in 1:length(ss$node)){
+		if(ss$time[n] == 0){
+			tmp = "root"
+		}else{
+			noden = ss$node[n]
+			dn = subset(ss, node==noden)$diversification
+			ancestors = Ancestors(tree, n)
+			parent = max(ancestors[ancestors %in% ss$node])
+			dp = subset(ss, node==parent)$diversification
+			tmp = ifelse(dn > dp, "growth", "decay")
+		}
+		data$change[which(data$file==f & data$index == ss$index[n])] = tmp
 	}
-	change = c(change, changeF)
-}; rm(f, dirRoot, clade, ss, tree, changeF, n, noden, dn, ancestors, parent, dp, tmp); cat("\rDone", rep(" ", 100), "\n")
+}; rm(f, dirRoot, clade, ss, tree, n, noden, dn, ancestors, parent, dp, tmp); cat("\rDone", rep(" ", 100), "\n")
 
-data$change = change; rm(change)
+table(data$change)
 data$change = factor(data$change, levels=c("root", "growth", "decay"))
 
-# Remove shifts happening in 4 or less phylogenetic trees during a 10 years span
+# Remove shifts happening in less than half of the phylogenetic trees
 # This is done to compensate the low ESS for big clades (e.g.: Holozoa, Nucletmycea and Alveolata)
-span = 50
+data$run = data$file %>% sub("\\/[^\\/]+$", "", .) %>% sub("\\/bamm/.*", "", .)
 data$replicates = 0
-for(c in unique(data$clade)){
-	for(d in unique(data$div)){
-		ss = subset(data, clade == c & div == d)
-		ss = subset(ss, change!="root")
-		sequence = seq(min(ss$timeRev), 0, span)
-		for(s in 1:(length(sequence)-1)){
-			cat("\r  Working on ", c, ": Analyzing '", d, "' diversity: ", round(s/length(sequence)*100), "%", sep="")
-			sss = subset(ss, timeRev >= sequence[s] & timeRev <= sequence[s+1])
-			if(nrow(sss) > 0){
-				for(n in 1:nrow(sss)){
-					data$replicates[which(data$node==sss$node[n] & data$file==sss$file[n])] = length(unique(sss$file))
+# The following loop will take several minutes/hours...
+for(cl in unique(data$clade)){
+	# To do so, we first get the tip.names of every shift, in order to compare them all
+	cat("  Working on (", grep(cl, unique(data$clade)), "/", length(unique(data$clade)), ") ", cl, "                    \n", sep="")
+	ss = subset(data, clade==cl)
+	dirTrees = grep(cl, files$trees, value=TRUE); trees = list()
+	for(d in dirTrees){trees[[d]] = read.tree(d)}
+	for(d in unique(ss$div)){
+		cat("    Analyzing '", d, "' diversity estimate\n", sep="")
+		sss = subset(ss, div==d)
+		childs = list()
+		for(n in 1:nrow(sss)){
+			cat("\r      Getting tip names ", round(n/nrow(sss)*100), "%", sep="", end="")
+			tree = trees[[grep(sss$run[n], names(trees))]]
+			childs[[n]] = tree$tip.label[unlist(Descendants(tree, sss$node[n], type="tips"))]
+		}; cat("\n")
+		# Now we find those nodes that contain at least 90% of the sequences in our given node
+		for(n in 1:length(childs)){
+			cat("\r      Counting replicated nodes ", round(n/length(childs)*100), "%", sep="", end="")
+			tmp = childs[[n]]; rep = 1
+			done = FALSE
+			r = sss$run[n]
+			for(N in 1:length(childs)){
+				if(!sss$run[N] %in% r){done = FALSE; r = c(r, sss$run[N])}
+				if(sss$run[n] != sss$run[N] & !done){
+					count = sum(tmp %in% childs[[N]])
+					if(count > length(tmp)*0.95){
+						rep = rep + 1
+						done = TRUE
+					}	
 				}
 			}
-		}
-	}; cat("\r  Working on ", c, ": Done                              \n", sep="")
-}; rm(c, d, ss, sequence, s, sss, n)
-
+			data$replicates[which(data$file == sss$file[n] & data$div == d & data$node == sss$node[n] & data$index == sss$index[n])] = rep
+		}; cat("\n")
+	}
+}; rm(cl, ss, sss, childs, dirTrees, d, trees, n, done, r, N, tree, tmp, count, rep); cat("Done")
 
 table(data$replicates)
-
-
-
 
 # And plot
 geos <- subset(geo, time>-max(data$time))
@@ -216,10 +225,10 @@ data$clade = factor(data$clade, levels=c("Amoebozoa","Nucletmycea","Holozoa","Me
 # And export
 pdf(paste0(files$dirPlots, "/", files$shiftsPlot), width=11.69, height=8.27, paper='special'); plot(shifts); dev.off()
 
-# And now only shifts happening in the proterozoic
-geos <- subset(geo, time < -540 & time>-max(data$time))
+# Now only shifts happening in the proterozoic
+geos <- subset(geo, time < -251 & time>-max(data$time))
 
-(shiftsP = ggplot(subset(data, timeRev < -541), aes(x=timeRev, y=clade, fill=change))+
+(shiftsP = ggplot(subset(data, timeRev < -251), aes(x=timeRev, y=clade, fill=change))+
 		geom_vline(xintercept = geos$time, color="lightgrey") +
 		geom_violin(scale="width", alpha=0.4)+
 		facet_wrap(~div, ncol=1)+
@@ -232,6 +241,66 @@ geos <- subset(geo, time < -540 & time>-max(data$time))
 
 pdf(paste0(files$dirPlots, "/", sub("\\.pdf", "_proterozoic.pdf", files$shiftsPlot)), width=11.69, height=8.27, paper='special'); plot(shiftsP); dev.off()
 
-#
 
+
+
+
+
+# And now with at least half of the replicates
+datas = data.frame()
+for(cl in unique(data$clade)){
+	ss = subset(data, clade==cl)
+	for(d in unique(ss$div)){
+		sss = subset(ss, div==d)
+		repMax=length(unique(sss$file))*0.75
+		datas = rbind(datas, subset(sss, replicates >= repMax))
+	}
+}; rm(cl, ss, d, sss, repMax)
+table(datas$replicates)
+# repMax = length(unique(subset(data, clade=="Alveolata" & div=="min")$file))/2
+
+# tmp = subset(datas, timeRev < -541)
+tmp = subset(datas, timeRev < -251)
+
+# geos <- subset(geo, time < -540 & time>-max(data$time))
+geos <- subset(geo, time < -251 & time>-max(data$time))
+# geos = subset(geo, time>-max(tmp$time))
+
+(shiftsPr = ggplot(tmp, aes(x=timeRev, y=clade, fill=change))+
+		geom_vline(xintercept = geos$time, color="lightgrey") +
+		geom_violin(scale="width", alpha=0.4)+
+		facet_wrap(~div, ncol=1)+
+		scale_x_continuous(breaks=seq(-max(round(data$time, -2)), 0, 100),
+						   minor_breaks=seq(-max(round(data$time, -2)), 0, 50)) +
+		labs(title="Shifts in diversification rates on the Proterozoic",
+			 y="Clades", x="Time before present (Ma)")+
+		scale_fill_manual(values=c("grey20", "springgreen4", "orangered4")) +
+		theme_classic())
+# geom_violin; scale: if "area" (default), all violins have the same area (before trimming the tails). If "count", areas are scaled proportionally to the number of observations. If "width", all violins have the same maximum width.
+pdf(paste0(files$dirPlots, "/", sub("\\.pdf", "_proterozoic.pdf", files$shiftsPlot)), width=11.69, height=8.27, paper='special'); plot(shiftsPr); dev.off()
+
+
+# And finally, one file at a time
+pdf(paste0(files$dirPlots, "/", sub("\\_violin.pdf", "Runs.pdf", files$shiftsPlot)), width=11.69, height=8.27, paper='special')
+for(r in unique(datas$run)){
+	cat("  Plotting", r, "\n")
+	tmp = subset(datas, run == r)
+	geos <- subset(geo, time>-max(tmp$time))
+	# tmp = subset(tmp, timeRev < -251)
+	# geos <- subset(geos, time < -251)
+	shiftsR = ggplot(tmp, aes(x=timeRev, y=clade, size=diversification, colour=change))+
+		geom_vline(xintercept = geos$time, color="lightgrey") +
+		geom_point(alpha=0.4)+
+		facet_wrap(~div, ncol=1)+
+		scale_x_continuous(breaks=seq(-max(round(data$time, -2)), 0, 100),
+						   minor_breaks=seq(-max(round(data$time, -2)), 0, 50)) +
+		labs(title=paste0("Shifts in diversification rates: run ", r),
+			 y="Clades", x="Time before present (Ma)")+
+		scale_colour_manual(values=c("grey20", "springgreen4", "orangered4")) +
+		theme_classic()
+	plot(shiftsR)
+}; rm(r, tmp); cat("Done\n")
+dev.off()
+
+#
 #----  
